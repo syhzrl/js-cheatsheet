bbee3e2bdd81f083b625a8f551e32f0bdee31da9
syhzrl <syhzrl@gmail.com>
2022-11-27 18:14:07 +0800

    Created card content for "String Methods"

--- src/assets/contents/index.ts
+++ src/assets/contents/index.ts
@@ -2,6 +2,7 @@ import { CardContent } from 'entity/Card';
 
 import variablesContent from './Variables';
 import functionsContent from './Functions';
+import stringContent from './String';
 
 interface Content {
     title: string;
@@ -10,13 +11,19 @@ interface Content {
 }
 
 const contents: Content[] = [{
-    title: 'Variables',
-    desc: 'Variables can be declared in 2 ways',
-    content: variablesContent,
-}, {
     title: 'Functions',
     desc: 'Functions can be declared in 2 ways',
     content: functionsContent,
+},
+{
+    title: 'Variables',
+    desc: 'Variables can be declared in 2 ways',
+    content: variablesContent,
+},
+{
+    title: 'String Methods',
+    desc: 'Strings can be manipulated with different methods',
+    content: stringContent,
 }];
 
 export default contents;
