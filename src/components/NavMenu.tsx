8c35ad5600c0444a800b9fb1bcb3ec993debc56e
syhzrl <syhzrl@gmail.com>
2022-11-27 12:47:15 +0800

    Added javascript icons and updated nav menu

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react';
+import JsIcon from 'assets/icons/js.svg';
 
 interface NavMenuButtonProps {
     label: string;
@@ -10,7 +11,7 @@ const NavMenuButton: FunctionComponent<NavMenuButtonProps> = (props: NavMenuButt
     return (
         <button
             type='button'
-            className='bg-transparent p-3 hover:bg-[#dec91b] hover:text-bgPrimary transition-all duration-500 text-xl font-bold rounded-lg w-full'
+            className='bg-transparent p-2 hover:bg-[#dec91b] hover:text-bgPrimary transition-all duration-300 text-xl rounded-lg w-full'
         >
             <p>
                 {label}
@@ -33,12 +34,23 @@ const NavMenu: FunctionComponent = () => {
     ];
 
     return (
-        <div className='bg-[#1e1d1d] text-white w-1/5 sticky top-0 flex flex-col items-center space-y-1 h-screen'>
-            <div className='text-jsYellow p-4 flex justify-center items-center w-full text-xl font-bold'>
-                Javascript Cheatsheet
+        <div className='bg-[#1e1d1d] text-white w-1/5 sticky top-0 flex flex-col items-center h-screen'>
+            <div className='text-jsYellow p-4 text-2xl flex flex-col justify-center items-center w-full space-y-3'>
+                <div className='flex justify-center items-end space-x-3'>
+                    <JsIcon
+                        height={40}
+                        width={40}
+                    />
+
+                    <p>
+                        Cheatsheet
+                    </p>
+                </div>
+
+                <div className='w-full border-b border-jsYellow' />
             </div>
 
-            <div className='p-4 flex flex-col space-y-3'>
+            <div className='p-4 flex flex-col space-y-2'>
                 {navMenuButtonsLabel.map(item => {
                     return (
                         <NavMenuButton key={item} label={item} />
