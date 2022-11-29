3a5519cb6673493c42fc59c3dffb534cfe6de73a
syhzrl <syhzrl@gmail.com>
2022-11-29 18:20:00 +0800

    Changed ui layout and simplified card contents

--- src/assets/contents/Number.ts
+++ src/assets/contents/Number.ts
@@ -1,83 +1,37 @@
 import { CardContent } from 'entity/Card';
 
-const toStringCode = `const num = 123;
-console.log(num.toString()); //"123"`;
-
-const toExponentialCode = `const num = 9.656;
-console.log(num.toExponential(4)); //"9.6560e+0"`;
-
-const toFixedCode = `const num = 9.656;
-console.log(num.toFixed(4)); //"9.6560"`;
-
-const toPrecisionCode = `const num = 9.656;
-console.log(num.toPrecision(3)); //"9.66"`;
-
-const addCode = `const num1 = 10;
-const num2 = 25;
-console.log(num1 + num2); //35`;
-
-const subCode = `const num1 = 10;
-const num2 = 3;
-console.log(num1 - num2); //7`;
-
-const multiCode = `const num1 = 10;
+const arithmeticsCode = `const num1 = 10;
 const num2 = 2;
-console.log(num1 * num2); //20`;
 
-const divCode = `const num1 = 10;
-const num2 = 2;
-console.log(num1 / num2); //5`;
+num1 + num2; //12
+num1 - num2; //8
+num1 * num2; //20
+num1 / num2; //5
+num1 += 1; //increment
+num1 -= 1; //decrement
 
-const exponentCode = `const num1 = 14;
-const num2 = 2;
-console.log(num1 ** num2); //196`;
+//exponent
+num1 ** num2; //100
+//modulus ( remainder )
+num1 % num2; //0`;
 
-const modulusCode = `const num1 = 10;
-const num2 = 2;
-console.log(num1 % num2); //0`;
+const extraCode = `const num = 9.656;
 
-const incrementCode = `let num = 10;
-console.log(10 += 1); //11`;
-
-const decrementCode = `let num = 10;
-console.log(10 -= 1); //9`;
+//toString
+num.toString(); //"9.656"
+//toExponential
+num.toExponential(4); //"9.6560e+0"
+//toFixed
+num.toFixed(4); //"9.6560"
+//toPrecision
+num.toPrecision(3); //"9.66"`;
 
 const numbersContent: CardContent[] = [{
-    text: 'To String',
-    code: toStringCode,
+    text: 'Arithmetics',
+    code: arithmeticsCode,
 }, {
-    text: 'To Exponential',
-    code: toExponentialCode,
-}, {
-    text: 'To Fixed',
-    code: toFixedCode,
-}, {
-    text: 'To Precision',
-    code: toPrecisionCode,
-}, {
-    text: 'Addition',
-    code: addCode,
-}, {
-    text: 'Subtraction',
-    code: subCode,
-}, {
-    text: 'Multiplication',
-    code: multiCode,
-}, {
-    text: 'Division',
-    code: divCode,
-}, {
-    text: 'Exponent',
-    code: exponentCode,
-}, {
-    text: 'Modulus (Remainder)',
-    code: modulusCode,
-}, {
-    text: 'Increment',
-    code: incrementCode,
-}, {
-    text: 'Decrement',
-    code: decrementCode,
+    text: 'Extras',
+    code: extraCode,
 }];
 
 export default numbersContent;
--- src/assets/contents/String.ts
+++ src/assets/contents/String.ts
@@ -1,82 +1,53 @@
 import { CardContent } from 'entity/Card';
 
-const lengthCode = `const name='Bob Jenkins';
-console.log(name.length); //11`;
+const mutatingCode = `//these methods returns a new string
 
-const sliceCode = `//slice(start, end)
-const text = 'Apple, Banana, Kiwi';
-console.log(text.slice(7)); //Banana, Kiwi `;
+const text = 'Topgun Maverick';
 
-const subStringCode = `const text = 'Apple, Banana, Kiwi';
-console.log(str.substring(7, 13)); //Banana`;
+//slice(start, end)
+text.slice(6); //Maverick
 
-const replaceCode = `const text = 'I live in Japan';
-const newText = text.replace('Japan', 'Malaysia');
-console.log(newText); //I live in Malaysia`;
+//substring(start, end)
+text.substring(0, 6); //Topgun
 
-const upperLowerCode = `const text = 'Topgun Maverick';
-console.log(text.toLowerCase()); //topgun maverick
-console.log(text.toUpperCase()); //TOPGUN MAVERICK`;
+//replace(stringToReplace, replacement)
+text.replace('Maverick', 'Rooster'); //Topgun Rooster
 
-const concatCode = `const text1 = 'Topgun';
-const text2 = 'Maverick';
-console.log(text1.concat(' ', text2)); //Topgun Maverick`;
+//concat
+text.concat('✈'); //Topgun Maverick✈
 
-const trimCode = `const text = '    Astronaut    ';
-console.log(text.trim()); //Astronaut
-console.log(text.trimStart()); //"Astronaut    "
-console.log(text.trimEnd()); //"    Astronaut"`;
+//split ( returns an array )
+text.split(' '); //['Topgun', 'Maverick'];
 
-const padCode = `const text = 'Coffee';
-console.log(text.padStart(4, 'x')); //xxxCoffee
-console.log(text.padEnd(4, 'x')); //Coffeexxx`;
+//toUpperCase & toLowerCase
+text.toLowerCase(); //topgun maverick
+text.toUpperCase(); //TOPGUN MAVERICK
 
-const charCode = `const text = "Anime Fan";
-console.log(text.charAt(4)) //e;
-console.log(text.charCodeAt(4)) //101;`;
+//trimming
+const text = '    Pilot    ';
+text.trim(); //Pilot
+text.trimStart(); //"Pilot    "
+text.trimEnd(); //"    Pilot"
 
-const splitCode = `const text = "Split, String";
-console.log(text.split(', ')); //["Split", " String"]`;
+//padding
+text.padStart(4, 'x'); //xxxTopgun Maverick
+text.padEnd(4, 'x'); //Topgun Maverickxxx`;
+
+const extrasCode = `const text = 'Topgun Maverick';
+
+//length
+text.length; //15
+
+//charAt & charCodeAt
+text.charAt(4); //u
+text.charCodeAt(4); //117`;
 
 const stringContent: CardContent[] = [{
-    text: 'Length',
-    code: lengthCode,
-},
-{
-    text: 'Slice',
-    code: sliceCode,
-},
-{
-    text: 'Substring',
-    code: subStringCode,
-},
-{
-    text: 'Replace',
-    code: replaceCode,
-},
-{
-    text: 'To Lower Case & To Upper Case',
-    code: upperLowerCode,
-},
-{
-    text: 'Concat',
-    code: concatCode,
-},
-{
-    text: 'Trim, Trim Start & Trim End',
-    code: trimCode,
-},
-{
-    text: 'Pad Start & Pad End',
-    code: padCode,
-},
-{
-    text: 'Char At & Char Code At',
-    code: charCode,
-},
-{
-    text: 'Split',
-    code: splitCode,
+    text: 'Mutations',
+    code: mutatingCode,
+}, {
+    text: 'Extras',
+    code: extrasCode,
 }];
 
 export default stringContent;
